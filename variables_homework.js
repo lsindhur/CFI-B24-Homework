44059cb1172ceef5a58cce2c9322051fdf39a2b4
lsindhu <sindhu151095@gmail.com>
2024-06-22 15:08:03 +0530

    JS variables lecture homework 22/6

---
+++ variables_homework.js
@@ -0,0 +1,17 @@
+// creating sign up page
+var firstName = 'Sindhura'
+var lastName = 'Annamaneni'
+var age = 27
+var email = 'sindhuxyz@gmail.com'
+var course = 'Fullstack web development'
+var city = 'Hyderabad'
+const password = '1234567'
+var isLoggedIn = true
+
+console.log(`
+ Full Name of the user is ${firstName + lastName}
+ and her email is ${email}
+ uses the password ${password}
+ is from ${city}
+ and is studying ${course}
+    `)
